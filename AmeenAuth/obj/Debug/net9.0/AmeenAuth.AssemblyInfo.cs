9e59c1dce05fe0ce441a910142ec405b0fcffba5
abdallahelghobary <abdualahghobary@gmail.com>
2026-05-01 16:59:16 +0300

    Add Cookies login & finish login Page

--- AmeenAuth/obj/Debug/net9.0/AmeenAuth.AssemblyInfo.cs
+++ AmeenAuth/obj/Debug/net9.0/AmeenAuth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmeenAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8191397705f133667b5520479fb3cab146913644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2388b06217fec8d331a9deff956368b9f44bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmeenAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmeenAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
